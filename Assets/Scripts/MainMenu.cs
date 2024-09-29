c1dbc99afb9be1f3f1e87f38a7fbb677b8a261e4
Mohammed Adil Ahmed <adil12jnv@gmail.com>
2024-09-28 20:26:14 -0500

    added player movement

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,170 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject options_menu;
     public GameObject credit_menu;
+    public Button playButton;
+    public Button optionsButton;
+    public Button creditsButton;
+    public Button quitButton;
+    public Button optionsBackButton;
+    public Button creditsBackButton;
+
+    private Button[] mainMenuButtons;
+    private int currentIndex = 0;
+    private bool canNavigate = true;
+    public float inputDelay = 0.2f;
+
+    private enum MenuState { MainMenu, OptionsMenu, CreditsMenu }
+    private MenuState currentMenuState = MenuState.MainMenu;
+
+    public Material normalMaterial;
+    public Material highlightedMaterial;
+
+    private void Start()
+    {
+        mainMenuButtons = new Button[] { playButton, creditsButton, optionsButton, quitButton };
+
+        EventSystem.current.SetSelectedGameObject(mainMenuButtons[currentIndex].gameObject);
+
+        foreach (Button btn in mainMenuButtons)
+        {
+            SetButtonMaterial(btn, normalMaterial);
+        }
+
+        options_menu.SetActive(false);
+        credit_menu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (!canNavigate)
+            return;
+
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        switch (currentMenuState)
+        {
+            case MenuState.MainMenu:
+                HandleMainMenuInput(verticalInput, horizontalInput);
+                break;
+            case MenuState.OptionsMenu:
+                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Cancel"))
+                {
+                    options_menu_exit();
+                }
+                break;
+            case MenuState.CreditsMenu:
+
+                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Cancel"))
+                {
+                    credits_menu_exit();
+                }
+                break;
+        }
+    }
+
+    private void HandleMainMenuInput(float verticalInput, float horizontalInput)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || verticalInput > 0.5f)
+        {
+            currentIndex -= 2;
+            if (currentIndex < 0)
+                currentIndex += mainMenuButtons.Length;
+
+            UpdateSelection();
+            StartCoroutine(InputDelay());
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) || verticalInput < -0.5f)
+        {
+            currentIndex += 2;
+            if (currentIndex >= mainMenuButtons.Length)
+                currentIndex -= mainMenuButtons.Length;
+
+            UpdateSelection();
+            StartCoroutine(InputDelay());
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || horizontalInput < -0.5f)
+        {
+            if (currentIndex % 2 == 0)
+            {
+                currentIndex += 1;
+            }
+            else
+            {
+                currentIndex -= 1;
+            }
+
+            UpdateSelection();
+            StartCoroutine(InputDelay());
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || horizontalInput > 0.5f)
+        {
+            if (currentIndex % 2 == 0)
+            {
+                currentIndex += 1;
+            }
+            else
+            {
+                currentIndex -= 1;
+            }
+
+            UpdateSelection();
+            StartCoroutine(InputDelay());
+        }
+
+        // Select the current button with Enter key or gamepad "Submit" button
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("Submit"))
+        {
+            mainMenuButtons[currentIndex].onClick.Invoke();
+        }
+    }
+
+    private void UpdateSelection()
+    {
+        // Update the selected GameObject in the EventSystem
+        EventSystem.current.SetSelectedGameObject(mainMenuButtons[currentIndex].gameObject);
+
+        // Update button materials
+        for (int i = 0; i < mainMenuButtons.Length; i++)
+        {
+            if (i == currentIndex)
+            {
+                SetButtonMaterial(mainMenuButtons[i], highlightedMaterial); // Set to highlighted material
+            }
+            else
+            {
+                SetButtonMaterial(mainMenuButtons[i], normalMaterial); // Set to normal material
+            }
+        }
+    }
+
+    private void SetButtonMaterial(Button button, Material material)
+    {
+        Image img = button.GetComponent<Image>();
+        if (img != null)
+        {
+            img.material = material;
+        }
+    }
+
+    IEnumerator InputDelay()
+    {
+        canNavigate = false;
+        yield return new WaitForSeconds(inputDelay);
+        canNavigate = true;
+    }
 
     public void play_button_action()
     {
@@ -16,21 +174,61 @@ public class MainMenu : MonoBehaviour
     public void options_button_enter()
     {
         options_menu.SetActive(true);
+        currentMenuState = MenuState.OptionsMenu;
+
+        // Disable main menu buttons
+        foreach (Button btn in mainMenuButtons)
+        {
+            btn.interactable = false;
+        }
+
+        // Set focus to the back button in options menu
+        EventSystem.current.SetSelectedGameObject(optionsBackButton.gameObject);
     }
 
     public void options_menu_exit()
     {
         options_menu.SetActive(false);
+        currentMenuState = MenuState.MainMenu;
+
+        // Enable main menu buttons
+        foreach (Button btn in mainMenuButtons)
+        {
+            btn.interactable = true;
+        }
+
+        // Return focus to previously selected main menu button
+        UpdateSelection();
     }
 
     public void credits_menu_enter()
     {
         credit_menu.SetActive(true);
+        currentMenuState = MenuState.CreditsMenu;
+
+        // Disable main menu buttons
+        foreach (Button btn in mainMenuButtons)
+        {
+            btn.interactable = false;
+        }
+
+        // Set focus to the back button in credits menu
+        EventSystem.current.SetSelectedGameObject(creditsBackButton.gameObject);
     }
 
     public void credits_menu_exit()
     {
         credit_menu.SetActive(false);
+        currentMenuState = MenuState.MainMenu;
+
+        // Enable main menu buttons
+        foreach (Button btn in mainMenuButtons)
+        {
+            btn.interactable = true;
+        }
+
+        // Return focus to previously selected main menu button
+        UpdateSelection();
     }
 
     public void quit_button_action()
--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,87 +1,170 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Scripting.APIUpdating;
-using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
 
 public class PlayerMovements : MonoBehaviour
 {
     public float player_speed;
-
     private Vector2 direction;
-    
     public float dash_range;
     private Vector2 controller_input;
-
-    public enum Facing {UP, DOWN, LEFT, RIGHT};
-
+    public enum Facing { UP, DOWN, LEFT, RIGHT };
     public Facing FacingDir = Facing.DOWN;
-    
-    void Update()
+    private bool facingRight = true;
+
+    private Animator animator;
+
+    public Transform characterVisuals;
+
+    void Start()
     {
-         input();
-         move();
-         
+        animator = GetComponentInChildren<Animator>();
+        animator.SetBool("isHeavyAttack", false);
+        animator.SetBool("isLightAttack", false);
+        animator.SetBool("isWalking", false);
+
+        if (characterVisuals == null)
+        {
+            characterVisuals = animator.transform;
+        }
     }
 
-    private void input(){
+    void Update()
+    {
+        input();
+        move();
+    }
+
+    private void input()
+    {
         direction = Vector2.zero;
-        
-        if(Input.GetKey(KeyCode.W)){
+
+        if (Input.GetKey(KeyCode.W))
+        {
             direction += Vector2.up;
             FacingDir = Facing.UP;
         }
-        
-        if(Input.GetKey(KeyCode.A)){
+
+        if (Input.GetKey(KeyCode.A))
+        {
             direction += Vector2.left;
             FacingDir = Facing.LEFT;
         }
-        
-        if(Input.GetKey(KeyCode.S)){
+
+        if (Input.GetKey(KeyCode.S))
+        {
             direction += Vector2.down;
             FacingDir = Facing.DOWN;
         }
 
-        if(Input.GetKey(KeyCode.D)){
+        if (Input.GetKey(KeyCode.D))
+        {
             direction += Vector2.right;
             FacingDir = Facing.RIGHT;
         }
 
         Gamepad gamepad = Gamepad.current;
-        
+
         if (gamepad != null)
         {
-            controller_input = gamepad.leftStick.ReadValue(); 
+            controller_input = gamepad.leftStick.ReadValue();
             direction += controller_input;
+
+            if (controller_input != Vector2.zero)
+            {
+                if (Mathf.Abs(controller_input.x) > Mathf.Abs(controller_input.y))
+                {
+                    if (controller_input.x > 0)
+                    {
+                        FacingDir = Facing.RIGHT;
+                    }
+                    else
+                    {
+                        FacingDir = Facing.LEFT;
+                    }
+                }
+                else
+                {
+                    if (controller_input.y > 0)
+                    {
+                        FacingDir = Facing.UP;
+                    }
+                    else
+                    {
+                        FacingDir = Facing.DOWN;
+                    }
+                }
+            }
+
+            if (gamepad.buttonSouth.wasPressedThisFrame)
+            {
+                Dash();
+            }
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space) )
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-                
-                Vector2 target_pos = Vector2.zero;
-
-                if (FacingDir == Facing.UP){
-                    target_pos += Vector2.up;
-                }
-                else if(FacingDir == Facing.DOWN)
-                {
-                    target_pos += Vector2.down;
-                }
-                else if(FacingDir == Facing.RIGHT)
-                {
-                    target_pos += Vector2.right;
-                }
-                else if (FacingDir == Facing.LEFT){
-                    target_pos += Vector2.left;
-                }
-                transform.Translate(target_pos * dash_range);
+            Dash();
         }
     }
 
-    private void move(){
-        transform.Translate(direction * player_speed * Time.deltaTime);
+    private void Dash()
+    {
+        Vector2 target_pos = Vector2.zero;
+
+        if (FacingDir == Facing.UP)
+        {
+            target_pos += Vector2.up;
+        }
+        else if (FacingDir == Facing.DOWN)
+        {
+            target_pos += Vector2.down;
+        }
+        else if (FacingDir == Facing.RIGHT)
+        {
+            target_pos += Vector2.right;
+        }
+        else if (FacingDir == Facing.LEFT)
+        {
+            target_pos += Vector2.left;
+        }
+
+        Vector3 dashMovement = new Vector3(target_pos.x, target_pos.y, 0);
+        transform.Translate(dashMovement * dash_range, Space.World);
     }
 
+    private void move()
+    {
+        if (direction != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);
+
+            if (direction.x > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (direction.x < 0 && facingRight)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        Vector3 movement = new Vector3(direction.x, direction.y, 0);
+
+        transform.Translate(movement.normalized * player_speed * Time.deltaTime, Space.World);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 rotation = characterVisuals.eulerAngles;
+        rotation.y += 180;
+        characterVisuals.eulerAngles = rotation;
+    }
 }
