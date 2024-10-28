0c821f67d02a1d169b4ba9d29aaac990e6c56f84
Mohammed Adil Ahmed <adil12jnv@gmail.com>
2024-10-28 08:43:52 -0500

    made changes to fix build

--- Assets/Scripts/ProceduralGeneration/RoomController.cs
+++ Assets/Scripts/ProceduralGeneration/RoomController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
-using UnityEditor.VersionControl;
+
 
 public class RoomController : MonoBehaviour
 {
--- Assets/Scripts/UIScreens/GameOverScreen.cs
+++ Assets/Scripts/UIScreens/GameOverScreen.cs
@@ -1,32 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build.Content;
 
 public class GameOverScreen : MonoBehaviour
 {
-    private PlayerMovements playerMovements;
-    private PlayerHealthManager playerHealth;
-
-    private void Start()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            playerMovements = player.GetComponent<PlayerMovements>();
-            playerHealth = player.GetComponent<PlayerHealthManager>();
-        }
-
-
-        if (playerMovements == null || playerHealth == null)
-        {
-            Debug.LogError("PlayerMovements or MainPlayerHealthManager scripts not found!");
-        }
-
-    }
     public void RestartButton()
     {
         SceneManager.LoadScene("Level-1", LoadSceneMode.Single);
