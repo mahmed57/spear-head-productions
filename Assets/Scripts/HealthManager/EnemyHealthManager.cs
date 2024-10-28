14e63de145545b7c8b6e45735611e72275a51b64
Mohammed Adil Ahmed <adil12jnv@gmail.com>
2024-10-28 18:30:37 -0500

    added health bars for enemies

--- Assets/Scripts/HealthManager/EnemyHealthManager.cs
+++ Assets/Scripts/HealthManager/EnemyHealthManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthManager:CharacterHealthManager
 {
      public float pushForce = 5f;
 
      private EnemyMovement enemyMovement;
-     private RageTankerMovement rage_tanker_movement;
+     public Slider health_bar_slider;
+
+    public GameObject health_bar;
 
     void Start()
     {
@@ -16,17 +19,16 @@ public class EnemyHealthManager:CharacterHealthManager
 
     public override void deal_damage(float damage, Vector2 attackPosition)
     {
+        health_bar.SetActive(true);
+
         base.deal_damage(damage, attackPosition);
 
         Vector2 pushDirection = (transform.position - new Vector3(attackPosition.x, attackPosition.y, transform.position.z)).normalized;
 
-
         enemyMovement.ApplyPushBack(pushDirection, pushForce);
-    
 
-
-        
-        
+        health_bar_slider.value = present_health / max_health;
+       
     }
 
 }
