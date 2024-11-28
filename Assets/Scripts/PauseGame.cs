d4339d6025f33df26c3fafe1f580f19936f42372
Mohammed Adil Ahmed <adil12jnv@gmail.com>
2024-11-27 22:19:19 -0600

    added gamepad controller for shop, pause menu and fixed bug with pause menu.

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class PauseGame : MonoBehaviour
@@ -9,19 +10,41 @@ public class PauseGame : MonoBehaviour
     bool pause_game = false;
     bool canUnpause = false;
 
+    public float start_time = 5;
+
+    public GameObject pause_menu;
+
     void Start()
     {
         shop.SetActive(false);
+        pause_menu.SetActive(false);
     }
+
 
     void Update()
     {
-        bool input_condition = Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.JoystickButton4);
-
-        if (!pause_game && input_condition)
+        bool input_condition_shop = Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.JoystickButton4);
+        bool input_condition_pause_menu = Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.JoystickButton5);
+        if(!pause_menu.activeSelf && !shop.activeSelf)
+        {
+            pause_game = false;
+            canUnpause=false;
+        }
+        if (!pause_game && (input_condition_shop|| input_condition_pause_menu))
         {
             Time.timeScale = 0;
-            shop.SetActive(true);
+
+            if (input_condition_pause_menu){
+                pause_menu.SetActive(true);
+                shop.SetActive(false);
+            }
+
+            else
+            {        
+                shop.SetActive(true);
+                pause_menu.SetActive(false);
+            }
+            
             pause_game = true;
             canUnpause = false;
         }
@@ -34,14 +57,21 @@ public class PauseGame : MonoBehaviour
                 return;
             }
 
-            bool input_condition_1 = Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.JoystickButton4);
-
-            if (input_condition_1)
+            bool input_condition_shop_1 = Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.JoystickButton4);
+            bool input_condition_pause_menu_1 = Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.JoystickButton5);
+            
+            if ((input_condition_shop_1 && shop.activeSelf) || (input_condition_pause_menu_1 && pause_menu.activeSelf))
             {
-                Time.timeScale = 1;
+                
                 shop.SetActive(false);
+                pause_menu.SetActive(false);
+                float future_time = Time.time + start_time;
+                
                 pause_game = false;
+
+                Time.timeScale = 1;
             }
+
         }
     }
 }
--- Assets/Scripts/UIScreens/PauseMenu.cs
+++ Assets/Scripts/UIScreens/PauseMenu.cs
@@ -6,38 +6,16 @@ public class PauseMenu : MonoBehaviour
     public static bool isPaused = false;
     public GameObject pauseMenuUI;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.X)) 
-        {
-            if (isPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-
     public void Resume()
     {
+        
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f; 
-        isPaused = false;
-    }
-
-    public void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f; 
-        isPaused = true;
     }
 
     public void LoadMainMenu()
     {
-        Time.timeScale = 1f; 
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu"); 
     }
 
