394b7f3384c5f3e1e816afd5f3d49e212dadd4eb
Mohammed Adil Ahmed <adil12jnv@gmail.com>
2024-12-03 15:25:45 -0600

    added enemies to boss room

---
+++ Assets/Scripts/Spawner/SpawnEnemiesSpawnPoints.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class SpawnEnemiesSpawnPoints : MonoBehaviour
+{
+    // Start is called before the first frame update    // List of spawn points set in the Unity Editor
+    public List<Transform> spawnPoints;
+
+    // Names of enemy prefabs in the Resources folder
+    private string[] enemyNames = { "Despair", "Misery", "RageTanker" };
+
+    public float cooldown = 10f;
+    
+    void Start()
+    {
+
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            // Wait for 10 seconds before spawning the next enemy
+            yield return new WaitForSeconds(cooldown - 5f);
+
+            // Select a random spawn point
+            int spawnIndex = Random.Range(0, spawnPoints.Count);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            // Select a random enemy
+            int enemyIndex = Random.Range(0, enemyNames.Length);
+            string enemyName = enemyNames[enemyIndex];
+
+            // Load the enemy prefab from the Resources folder
+            GameObject enemyPrefab = Resources.Load<GameObject>("Prefabs/" + enemyName);
+
+            if (enemyPrefab != null)
+            {
+                // Instantiate the enemy at the spawn point
+                Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            }
+            else
+            {
+                Debug.LogError("Enemy prefab not found: " + enemyName);
+            }
+
+            if(cooldown < 10)
+            {
+                    cooldown = cooldown + 5f;
+            }
+
+            
+        }
+    }
+}
